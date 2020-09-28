3a4540b5ac938b40efb88b87711134721468ec39
Odenn <kazuya@odenn.org>
2020-09-28 14:54:45 +0900

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# Noodle Timer
+This is kidding app...Hhhhh
+After Pour water , Cup noodle time counting helper...yeah :>  
+
+## Dependency  
+macOS Catalina 10.15.6  
+You can use bash or zsh  
+
+## Usage
+
+Tear open half of the paper/plastic lid.  
+Pour some boiling (100 C/212 F) water up to the inside line of the container.  
+Put the lid on and weigh down the lid with a set of chopsticks or a light plate.  
+Open laptop.  
+Open terminal.app  
+Input this command  
+
+```
+$ sh noodle.sh
+```
+ 
+Wait patiently for 2 to 3 minutes then stir the noodles well with chopsticks.  
+Done.  
+
+## License
+This software is released under the MIT License, see LICENSE.  
+
+## Authors
+kazuya yuda.  
+
+## References
+
+AppleScript で音を鳴らす
+https://dragox.exblog.jp/21614329/
+
+
+Mavericks コマンドラインキッチンタイマー「ゴージャスラーメン」オプション
+https://qiita.com/kaz-utashiro/items/72f9e4c089c46210dec4
+
---
+++ noodle.sh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+clear
+trap 'date' 2
+sh -c "/bin/echo -n \"How many minutes? 🍜 : \""
+read Sec
+echo =================================
+echo "Set ${Sec} minutes."
+view=$((Sec*60))
+
+echo =================================
+afplay /System/Library/Sounds/Hero.aiff
+sh -c "/bin/echo -n \"start time -> \""
+date "+%Y-%m-%d %H:%M:%S"
+echo =================================
+
+icon=("ー" "＼" "｜" "／" "ー" "＼" "｜" "／")
+count=0
+for i in `seq $view`
+do 
+    sleep 1
+    ESC=$(printf '\033')
+    echo "${ESC}[m ${ESC}[35m${icon[$count]}${ESC}[m ${ESC}[36m$((view-i)) seconds left...${ESC}[m ${ESC}[35m${icon[$count]}${ESC}[m" |
+    while read line; do
+        printf '\r%*s\r' ${lenLine:-${#line}}
+        printf "%s" "$line"
+        lenLine=${#line}
+    done
+    if [ $count = 7 ]; then
+	count=0
+    else
+	count=$((count + 1))
+    fi
+done
+echo ""
+echo =================================
+sh -c "/bin/echo -n \"complete time -> \""
+date "+%Y-%m-%d %H:%M:%S"
+echo =================================
+osascript -e "display notification \"$Sec 分経過しました.\" with title \"🍜 完成しました.\""
+for i in `seq 6`
+do
+afplay /System/Library/Sounds/Purr.aiff
+done
